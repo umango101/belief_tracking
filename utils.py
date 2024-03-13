7465d9f1f44b4d631cc531f38796bd07c7ace7bb
Nikhil Prakash <nikhil07prakash@gmail.com>
2024-03-13 14:46:04 -0400

    Werewolf game script

--- utils.py
+++ utils.py
@@ -1,6 +1,33 @@
+import os
+import random
 import json
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from typing import List, Dict, Any
+from transformers import StoppingCriteria
 from datasets import Dataset
+
+
+class NewLineStoppingCriteria(StoppingCriteria):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.newline_token_id = self.tokenizer.encode("\n")[-1]
+        self.txt_generated = False
+
+    def __call__(self, input_ids: Any, scores: Any) -> Any:
+        """
+        Checks if the current generated token is a new line character.
+        Returns a tensor of booleans with the same shape as scores,
+        indicating whether generation should stop for each sequence.
+        """
+        latest_token_ids = input_ids[:, -1]
+        if latest_token_ids == self.newline_token_id and self.txt_generated:
+            self.txt_generated = False
+            return True
+        else:
+            if latest_token_ids != self.newline_token_id:
+                self.txt_generated = True
+            return False
 
 
 def get_dataset(datafiles: list[str]) -> Dataset:
@@ -39,9 +66,7 @@ def collate_fn(model, tokenizer, examples) -> dict[str, torch.Tensor]:
         or model.config.architectures[0] == "LlaMAForCausalLM"
         or model.config.architectures[0] == "GemmaForCausalLM"
     ):
-        inputs["target"] = [
-            tokenizer.decode(tokenizer.encode(ex["target"])[1]) for ex in examples
-        ]
+        inputs["target"] = [tokenizer.decode(tokenizer.encode(ex["target"])[1]) for ex in examples]
     elif (
         model.config.architectures[0] == "MistralForCausalLM"
         or model.config.architectures[0] == "MixtralForCausalLM"
@@ -52,3 +77,124 @@ def collate_fn(model, tokenizer, examples) -> dict[str, torch.Tensor]:
     else:
         raise NotImplementedError
     return inputs
+
+
+def load_model_tokenzier(model_name: str, precision: str, device: torch.device):
+    if precision == "fp32":
+        model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+    elif precision == "fp16":
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(
+            device
+        )
+    elif precision == "int8":
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            device_map="auto",
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+        )
+    elif precision == "int4":
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            device_map="auto",
+            load_in_4bit=True,
+            torch_dtype=torch.float16,
+        )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+
+def compute_final_roles(players: List[Dict[str, str]]):
+
+    for player in players:
+        player["final_role"] = player["role"]
+
+    indices = [idx for idx, player in enumerate(players) if player["role"] != "Troublemaker"]
+    idx1, idx2 = random.sample(indices, 2)
+    players[idx1]["final_role"] = players[idx2]["role"]
+    players[idx2]["final_role"] = players[idx1]["role"]
+
+    return players
+
+
+def compute_role_description(players: List[Dict[str, str]]):
+    for idx, player in enumerate(players):
+        if player["role"] != "Troublemaker":
+            role_description = f"You are {player['name']}. You are playing Werewolf card game with your friends. Initially, you've been given the role of {player['role']}. First, understand the goals and actions of each player, then speak accordingly to increase your chances of winning."
+        else:
+            swapped_players = [p for p in players if p["role"] != p["final_role"]]
+
+            role_description = f"You are {player['name']}. You are playing Werewolf card game with your friends. Initially, you've been given the role of {player['role']}. During the night phase, you swapped {swapped_players[0]['name']} and {swapped_players[1]['name']}'s cards. Share this information during the Discussion Phase to identify the Werewolf. First, understand the goals and actions of each player, then speak accordingly to increase your chances of winning."
+
+        players[idx]["role_description"] = role_description
+    return players
+
+
+def ask_mental_state_questions(
+    model: Any,
+    tokenizer: Any,
+    device: torch.device,
+    players: List[Dict[str, str]],
+    game_description: str,
+    game_idx: int,
+    conversation: str,
+):
+
+    with torch.no_grad():
+        for player_idx in range(len(players)):
+            player = players[player_idx]
+            other_players = [p["name"] for p in players if p["name"] != player["name"]]
+            concatenated_other_players = " or ".join(other_players)
+
+            prompt = f"{game_description}\n\n{player['role_description']}\n\n"
+
+            if os.path.exists(f"/conversations/{game_idx}.txt"):
+                with open(f"/conversations/{game_idx}.txt", "r") as f:
+                    conversation = f.read()
+            else:
+                conversation = ""
+
+            prompt += f"DAY PHASE:\n{conversation}\n"
+            question = f"QUESTION: Who would you vote for ({concatenated_other_players} or don't know)?\nAnswer:"
+            prompt += question
+
+            inputs = tokenizer(prompt, return_tensors="pt").to(device)
+            outputs = model.generate(
+                **inputs,
+                max_new_tokens=5,
+                num_return_sequences=1,
+                temperature=0.00,
+                do_sample=False,
+                pad_token_id=tokenizer.eos_token_id,
+            )
+            response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+            own_vote = response[len(prompt) :].strip()
+
+            others_vote = []
+            for other_player in other_players:
+                concatenated_other_players = concatenated_other_players.replace(
+                    f"{other_player}", f"{player['name']}"
+                )
+
+                question = f"QUESTION: Who would {other_player} vote for ({concatenated_other_players} or don't know)?\nAnswer:"
+                prompt += question
+                inputs = tokenizer(prompt, return_tensors="pt").to(device)
+                outputs = model.generate(
+                    **inputs,
+                    max_new_tokens=5,
+                    num_return_sequences=1,
+                    temperature=0.00,
+                    do_sample=False,
+                    pad_token_id=tokenizer.eos_token_id,
+                )
+                response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+                others_vote.append(response[len(prompt) :].strip())
+
+                concatenated_other_players = concatenated_other_players.replace(
+                    f"{player['name']}", f"{other_player}"
+                )
+
+        return own_vote, others_vote
---
+++ werewolf/play.py
@@ -0,0 +1,107 @@
+import random
+import os
+import sys
+import torch
+import fire
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from utils import (
+    load_model_tokenzier,
+    compute_role_description,
+    NewLineStoppingCriteria,
+    compute_final_roles,
+    ask_mental_state_questions,
+)
+
+seed = 10
+random.seed(seed)
+torch.manual_seed(seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+player_names = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Heidi",
+    "Ivan",
+    "Judy",
+]
+player_roles = ["Villager", "Werewolf", "Troublemaker"]
+
+with open("game_description.txt", "r") as f:
+    game_description = f.read()
+
+# Delete all .txt and .cdv files in the conversations folder
+for file in os.listdir("./conversations"):
+    if file.endswith(".txt") or file.endswith(".csv"):
+        os.remove(os.path.join("./conversations", file))
+
+
+def play_werewolf(
+    model_name: str = "daryl149/llama-2-70b-chat-hf",
+    precision: str = "int4",
+    n_games: int = 10,
+    n_players: int = 3,
+    n_rounds: int = 5,
+):
+    model, tokenizer = load_model_tokenzier(model_name, precision, device)
+
+    for game_idx in range(n_games):
+        players = [
+            {"name": name, "role": role}
+            for name, role in zip(random.sample(player_names, n_players), player_roles * n_players)
+        ]
+        players = compute_final_roles(players)
+        players = compute_role_description(players)
+
+        stopping_criteria = NewLineStoppingCriteria(tokenizer)
+
+        with torch.no_grad():
+            for round_idx in range(n_rounds):
+                for player_idx in range(n_players):
+                    player = players[player_idx]
+                    prompt = f"{game_description}\n\n{player['role_description']}\n\n"
+
+                    if os.path.exists(f"/conversations/{game_idx}.txt"):
+                        with open(f"/conversations/{game_idx}.txt", "r") as f:
+                            conversation = f.read()
+                    else:
+                        conversation = ""
+
+                    prompt += f"DAY PHASE:\n{conversation}\n{player['name']} says:"
+
+                    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+                    outputs = model.generate(
+                        **inputs,
+                        max_new_tokens=100,
+                        num_return_sequences=1,
+                        temperature=0.00,
+                        do_sample=False,
+                        stopping_criteria=[stopping_criteria],
+                        early_stopping=True,
+                        pad_token_id=tokenizer.eos_token_id,
+                    )
+                    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+                    with open(f"./conversations/{game_idx}.txt", "a+") as f:
+                        f.write(f"{player['name']} says: {response[len(prompt) :]}\n")
+
+            own_vote, other_votes = ask_mental_state_questions(
+                model, tokenizer, device, players, game_description, game_idx, conversation
+            )
+            # Concatenate the votes of the other players
+            other_votes = [f"{player['name']}:{vote}" for player, vote in zip(players, other_votes)]
+
+            # Create a csv to store the game results
+            with open(f"./conversations/{game_idx}.csv", "w") as f:
+                f.write("Name,Role,Own Vote,Other Votes\n")
+                for player in players:
+                    f.write(f"{player['name']},{player['role']},{own_vote},{other_votes}\n")
+
+
+if __name__ == "__main__":
+    fire.Fire(play_werewolf)
