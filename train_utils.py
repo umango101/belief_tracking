23062bc6e27689bae1a38254c9dba89302ee1159
Nikhil Prakash <nikhil07prakash@gmail.com>
2024-06-02 19:56:52 -0400

    Finetuned model weight on old data

--- train_utils.py
+++ train_utils.py
@@ -32,11 +32,12 @@ class DataModule(LightningDataModule):
     def __init__(
         self,
         tokenizer,
-        data_path=None,
-        batch_size=None,
-        cutoff_len=None,
-        seed=None,
-        config=None,
+        data_path,
+        batch_size,
+        cutoff_len,
+        seed,
+        config,
+        current_dir,
         val_size=0,
         test_size=0,
     ):
@@ -50,7 +51,11 @@ class DataModule(LightningDataModule):
         self.cutoff_len = cutoff_len
         self.config = config
         self.prepare_data_per_node = False
+        self.current_dir = current_dir
         self.train_set = load_dataset("json", data_files=self.data_path)["train"]
+
+    def eval_metric(self):
+        return LabelClassification()
 
     def tokenize(self, prompts):
         results = self.tokenizer(
@@ -61,11 +66,76 @@ class DataModule(LightningDataModule):
         )
         return results
 
+    def preprocess_valid_data(self, data):
+        prev_sent_idx = 0
+        processed_data = []
+        example = []
+
+        for sentence in data:
+            sent_idx = int(sentence.split(" ")[0])
+            sentence = sentence[2:]
+
+            if sent_idx > prev_sent_idx:
+                example.append(sentence)
+            else:
+                context = "".join(example[:-1]).strip()
+                question = example[-1].split("\t")[0].strip()
+                answer = example[-1].split("\t")[1].strip()
+                processed_data.append(
+                    {
+                        "input": f"{context}\nQuestion: {question}\nAnswer:",
+                        "target": " " + answer,
+                    }
+                )
+                example = [sentence]
+
+            prev_sent_idx = sent_idx
+
+        return processed_data
+
+    def get_valid_data(self):
+        with open(
+            f"{self.current_dir}/data/SymbolicToM Datasets/Story Structure Robustness Test Sets/D1.txt",
+            "r",
+        ) as f:
+            valid_data = f.readlines()
+
+        with open(
+            f"{self.current_dir}/data/SymbolicToM Datasets/Story Structure Robustness Test Sets/D2.txt",
+            "r",
+        ) as f:
+            valid_data += f.readlines()
+
+        with open(
+            f"{self.current_dir}/data/SymbolicToM Datasets/Story Structure Robustness Test Sets/D3.txt",
+            "r",
+        ) as f:
+            valid_data += f.readlines()
+
+        processed_data = self.preprocess_valid_data(valid_data)
+        dataset = Dataset.from_list(processed_data)
+        return dataset
+
+    def valid_collate_fn(self, examples):
+        self.tokenizer.padding_side = "left"
+        inputs = self.tokenizer(
+            [ex["input"] for ex in examples],
+            return_tensors="pt",
+            padding=True,
+        )
+
+        inputs["target"] = torch.tensor(
+            [self.tokenizer.encode(ex["target"])[1] for ex in examples]
+        )
+
+        return inputs
+
     def train_collate_fn(self, batch):
         batch_size = len(batch)
+        self.tokenizer.padding_side = "right"
 
         full_prompts = [
-            f'{batch[idx]["context"]}Question:{batch[idx]["question"]}\nAnswer:{batch[idx]["answer"]}'
+            f'{batch[idx]["context"]}Question: {batch[idx]["question"]}\nAnswer: {batch[idx]["answer"]}'
             for idx in range(batch_size)
         ]
         tokenized_full_prompts = self.tokenize(full_prompts)
@@ -74,40 +144,42 @@ class DataModule(LightningDataModule):
         # Masking the input tokens
         for idx in range(batch_size):
             input_len = len(
-                self.tokenize(f'{batch[idx]["context"]}Question:{batch[idx]["question"]}\nAnswer:')[
-                    "input_ids"
-                ]
+                self.tokenize(
+                    f'{batch[idx]["context"]}Question: {batch[idx]["question"]}\nAnswer:'
+                )["input_ids"]
             )
+
             # masking the input tokens
-            tokenized_full_prompts["labels"][idx] = [-100] * input_len + tokenized_full_prompts[
-                "labels"
-            ][idx][input_len:]
+            tokenized_full_prompts["labels"][idx] = [
+                -100
+            ] * input_len + tokenized_full_prompts["labels"][idx][input_len:]
 
             # masking the padded tokens
             content_len = sum(tokenized_full_prompts["attention_mask"][idx])
-            tokenized_full_prompts["labels"][idx] = tokenized_full_prompts["labels"][idx][
-                :content_len
-            ] + [-100] * (len(tokenized_full_prompts["input_ids"][idx]) - content_len)
+            tokenized_full_prompts["labels"][idx] = tokenized_full_prompts["labels"][
+                idx
+            ][:content_len] + [-100] * (
+                len(tokenized_full_prompts["input_ids"][idx]) - content_len
+            )
 
         # Left truncate the input tokens if it exceeds the cutoff length
         for idx in range(batch_size):
             if len(tokenized_full_prompts["input_ids"][idx]) > self.cutoff_len:
                 diff = len(tokenized_full_prompts["input_ids"][idx]) - self.cutoff_len
-                tokenized_full_prompts["input_ids"][idx] = tokenized_full_prompts["input_ids"][idx][
-                    diff:
-                ]
+                tokenized_full_prompts["input_ids"][idx] = tokenized_full_prompts[
+                    "input_ids"
+                ][idx][diff:]
                 tokenized_full_prompts["attention_mask"][idx] = tokenized_full_prompts[
                     "attention_mask"
                 ][idx][diff:]
-                tokenized_full_prompts["labels"][idx] = tokenized_full_prompts["labels"][idx][diff:]
+                tokenized_full_prompts["labels"][idx] = tokenized_full_prompts[
+                    "labels"
+                ][idx][diff:]
 
         for key in tokenized_full_prompts.keys():
             tokenized_full_prompts[key] = torch.tensor(tokenized_full_prompts[key])
 
         return tokenized_full_prompts
-
-    def eval_metric(self):
-        return LabelClassification()
 
     def train_dataloader(self):
         if self.train_set is None:
@@ -118,6 +190,17 @@ class DataModule(LightningDataModule):
                 batch_size=self.batch_size,
                 collate_fn=self.train_collate_fn,
                 shuffle=True,
+            )
+
+    def val_dataloader(self):
+        self.val_set = self.get_valid_data()
+        if self.val_set is None:
+            return None
+        else:
+            return DataLoader(
+                self.val_set,
+                batch_size=8,
+                collate_fn=self.valid_collate_fn,
             )
 
 
@@ -149,13 +232,7 @@ class TrainingModule(LightningModule):
         self.hyperparams = kwargs
         self.ckpt_epochs = ckpt_epochs
         self.train_log_step = train_log_step
-
-    def save_pretrained(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-        print("Saving model to {}".format(path))
-        self.model.save_pretrained(path)
-        self.tokenizer.save_pretrained(path)
+        self.best_valid_acc = 0
 
     def on_train_start(self) -> None:
         seed_everything(self.seed)
@@ -180,13 +257,66 @@ class TrainingModule(LightningModule):
         # self.log("train_loss", loss, logger=True, prog_bar=True, on_step=True)
         return loss
 
-    def on_train_epoch_end(self):
-        try:
-            local_rank = torch.distributed.get_rank()
-        except RuntimeError:
-            local_rank = -1
-        if local_rank in [-1, 0]:
-            self.model.save_pretrained(os.path.join(self.output_dir, f"{self.current_epoch}_ckpt"))
+    def on_validation_start(self):
+        self.metric.reset()
+        self.all_outputs = []
+        self.all_targets = []
+        if self.log_valid_loss:
+            self.valid_losses = []
+        return super().on_validation_start()
+
+    def validation_step(self, batch, batch_idx):
+        input_ids, targets = (
+            batch["input_ids"],
+            batch["target"],
+        )
+        outputs = self.model(input_ids)
+        logits = outputs.logits[:, -1]
+        pred_token_ids = torch.argmax(logits, dim=-1)
+
+        self.all_outputs.append(pred_token_ids)
+        self.all_targets.append(targets)
+
+        metric_scores = self.metric(pred_token_ids, targets)
+
+        if self.log_valid_loss:
+            loss = self.model(**batch).loss
+            metric_scores["valid_loss"] = loss
+            self.valid_losses.append(loss.item())
+
+        self.log_dict(metric_scores, prog_bar=True, sync_dist=True)
+
+    def on_validation_end(self) -> None:
+        results = self.metric.compute()
+
+        if results["accuracy"] > self.best_valid_acc:
+            self.metric.best_valid_acc = results["accuracy"]
+            try:
+                local_rank = torch.distributed.get_rank()
+            except RuntimeError:
+                local_rank = 0
+
+            if local_rank in [-1, 0]:
+                results = {k: v.item() for k, v in results.items()}
+                if self.log_valid_loss:
+                    results["valid_loss"] = sum(self.valid_losses) / len(
+                        self.valid_losses
+                    )
+
+                with open(os.path.join(self.output_dir, "results.json"), "w") as f:
+                    json.dump(results, f, indent=4)
+                f.close()
+
+            save_path = os.path.join(self.output_dir, f"preds.txt")
+            with open(save_path, "w", encoding="utf-8") as f:
+                for output, target in zip(self.all_outputs, self.all_targets):
+                    f.write("Output: {}\n".format(self.tokenizer.decode(output)))
+                    f.write("Target: {}\n\n".format(self.tokenizer.decode(target)))
+            f.close()
+
+            self.model.save_pretrained(f"{self.output_dir}/checkpoints/")
+
+        return super().on_validation_end()
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.model.parameters(), **self.hyperparams)
@@ -214,7 +344,8 @@ def load_model_tokenizer(model_name: str):
         device_map=device_map,
     )
     tokenizer = AutoTokenizer.from_pretrained(
-        model_name, token="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", padding_size="right"
+        model_name,
+        token="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",
     )
 
     tokenizer.pad_token_id = tokenizer.eos_token_id
