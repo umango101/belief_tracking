effda5f554e1eed62de196de8a5f931f74a3da80
Nikhil Prakash <nikhil07prakash@gmail.com>
2025-03-24 23:29:30 -0400

    BigToM Alignment

--- utils.py
+++ utils.py
@@ -1310,3 +1310,256 @@ def get_state_tracing_exps(STORY_TEMPLATES,
         })
     
     return samples
+
+
+def get_tb_fb_answer(tb_ans, fb_ans):
+    diff_idx = 0
+    for i, (v, j) in enumerate(zip(tb_ans, fb_ans)):
+        if v != j:
+            diff_idx = i
+            break
+    
+    tb_ans = " ".join(tb_ans[diff_idx:])[:-1]
+    fb_ans = " ".join(fb_ans[diff_idx:])[:-1]
+
+    return tb_ans, fb_ans
+
+
+def get_bigtom_samples(
+    df_false,
+    df_true,
+    n_samples,
+    type="false_belief"
+):
+    true_stories, false_stories = [], []
+    for i in range(len(df_true)):
+        story = df_true.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_true.iloc[i]['answer']
+        distractor = df_true.iloc[i]['distractor']
+        true_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+
+    for i in range(len(df_false)):
+        story = df_false.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_false.iloc[i]['answer']
+        distractor = df_false.iloc[i]['distractor']
+        false_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+    
+    samples = []
+    instruction = "1. Track the belief of each character as described in the story. 2. A character's belief is formed only when they perform an action themselves or can observe the action taking place. 3. A character does not have any belief about the container or its content which they cannot observe directly. 4. To answer the question, predict only the final state of the queried container in fewest tokens possible, strictly based on the belief of the character, mentioned in the question. 5. Do not predict the entire sentence with character or container as the final output."
+
+    for idx in range(n_samples):
+        tb_ans = true_stories[idx]["answer"].split()
+        fb_ans = false_stories[idx]["answer"].split()
+
+        # Find the index of first word which is different in both answers
+        diff_idx = 0
+        for i, (v, j) in enumerate(zip(tb_ans, fb_ans)):
+            if v != j:
+                diff_idx = i
+                break
+        
+        tb_ans = " ".join(tb_ans[diff_idx:])[:-1]
+        fb_ans = " ".join(fb_ans[diff_idx:])[:-1]
+
+        if type == "true_belief":
+            question = true_stories[idx]["question"]
+            tb_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[idx]['story']}\nQuestion: {question}\nAnswer:"
+            samples.append({
+                "prompt": tb_prompt,
+                "answer": tb_ans
+            })
+        else:
+            question = false_stories[idx]["question"]
+            fb_prompt = f"Instructions: {instruction}\n\nStory: {false_stories[idx]['story']}\nQuestion: {question}\nAnswer:"
+            samples.append({
+                "prompt": fb_prompt,
+                "answer": fb_ans
+            })
+
+    return samples
+
+
+def get_bigtom_value_fetcher_exps(
+        df_false,
+        df_true,
+        n_samples,
+):
+    true_stories, false_stories = [], []
+    for i in range(len(df_true)):
+        story = df_true.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_true.iloc[i]['answer']
+        distractor = df_true.iloc[i]['distractor']
+        true_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+
+    for i in range(len(df_false)):
+        story = df_false.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_false.iloc[i]['answer']
+        distractor = df_false.iloc[i]['distractor']
+        false_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+    
+    samples = []
+    instruction = "1. Track the belief of each character as described in the story. 2. A character's belief is formed only when they perform an action themselves or can observe the action taking place. 3. A character does not have any belief about the container or its content which they cannot observe directly. 4. To answer the question, predict only the final state of the queried container in fewest tokens possible, strictly based on the belief of the character, mentioned in the question. 5. Do not predict the entire sentence with character or container as the final output."
+
+    for idx in range(n_samples):
+        tb_ans = true_stories[idx]["answer"].split()
+        fb_ans = false_stories[idx]["answer"].split()
+        tb_ans, fb_ans = get_tb_fb_answer(tb_ans, fb_ans)
+
+        question = true_stories[idx]["question"]
+        org_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[idx]['story']}\nQuestion: {question}\nAnswer:"
+        org_ans = tb_ans
+
+        # Sample a different story
+        random_sample_idx = random.choice(range(len(true_stories)))
+        while random_sample_idx == idx:
+            random_sample_idx = random.choice(range(len(true_stories)))
+        
+        tb_ans = true_stories[random_sample_idx]["answer"].split()
+        fb_ans = false_stories[random_sample_idx]["answer"].split()
+        tb_ans, fb_ans = get_tb_fb_answer(tb_ans, fb_ans)
+
+        question = true_stories[random_sample_idx]["question"]
+        alt_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[random_sample_idx]['story']}\nQuestion: {question}\nAnswer:"
+        alt_ans = tb_ans
+
+        samples.append({
+            "org_story": true_stories[idx]['story'],
+            "org_question": true_stories[idx]['question'],
+            "org_prompt": org_prompt,
+            "org_ans": org_ans,
+            "alt_story": true_stories[random_sample_idx]['story'],
+            "alt_question": true_stories[random_sample_idx]['question'],
+            "alt_prompt": alt_prompt,
+            "alt_ans": alt_ans,
+            "target": alt_ans
+        })
+
+    return samples
+
+
+def get_bigtom_answer_state_exps(
+        df_false,
+        df_true,
+        n_samples,
+):
+    true_stories, false_stories = [], []
+    for i in range(len(df_true)):
+        story = df_true.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_true.iloc[i]['answer']
+        distractor = df_true.iloc[i]['distractor']
+        true_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+
+    for i in range(len(df_false)):
+        story = df_false.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_false.iloc[i]['answer']
+        distractor = df_false.iloc[i]['distractor']
+        false_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+    
+    samples = []
+    instruction = "1. Track the belief of each character as described in the story. 2. A character's belief is formed only when they perform an action themselves or can observe the action taking place. 3. A character does not have any belief about the container or its content which they cannot observe directly. 4. To answer the question, predict only the final state of the queried container in fewest tokens possible, strictly based on the belief of the character, mentioned in the question. 5. Do not predict the entire sentence with character or container as the final output."
+
+    for idx in range(n_samples):
+        org_tb_ans = true_stories[idx]["answer"].split()
+        org_fb_ans = false_stories[idx]["answer"].split()
+        org_tb_ans, org_fb_ans = get_tb_fb_answer(org_tb_ans, org_fb_ans)
+
+        question = true_stories[idx]["question"]
+        org_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[idx]['story']}\nQuestion: {question}\nAnswer:"
+        org_ans = org_tb_ans
+
+        # Sample a different story
+        random_sample_idx = random.choice(range(len(false_stories)))
+        while random_sample_idx == idx:
+            random_sample_idx = random.choice(range(len(false_stories)))
+        
+        alt_tb_ans = true_stories[random_sample_idx]["answer"].split()
+        alt_fb_ans = false_stories[random_sample_idx]["answer"].split()
+        alt_tb_ans, alt_fb_ans = get_tb_fb_answer(alt_tb_ans, alt_fb_ans)
+
+        question = false_stories[random_sample_idx]["question"]
+        alt_prompt = f"Instructions: {instruction}\n\nStory: {false_stories[random_sample_idx]['story']}\nQuestion: {question}\nAnswer:"
+        alt_ans = alt_fb_ans
+
+        samples.append({
+            "org_story": false_stories[idx]['story'],
+            "org_question": false_stories[idx]['question'],
+            "org_prompt": org_prompt,
+            "org_ans": org_ans,
+            "alt_story": true_stories[random_sample_idx]['story'],
+            "alt_question": true_stories[random_sample_idx]['question'],
+            "alt_prompt": alt_prompt,
+            "alt_ans": alt_ans,
+            "target": org_fb_ans
+        })
+
+    return samples
+
+
+def get_bigtom_query_charac(
+    df_false,
+    df_true,
+    n_samples
+):
+    true_stories, false_stories = [], []
+    for i in range(len(df_true)):
+        story = df_true.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_true.iloc[i]['answer']
+        distractor = df_true.iloc[i]['distractor']
+        true_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+
+    for i in range(len(df_false)):
+        story = df_false.iloc[i]['story']
+        question = df_true.iloc[i]['question']
+        answer = df_false.iloc[i]['answer']
+        distractor = df_false.iloc[i]['distractor']
+        false_stories.append({"story": story, "question": question, "answer": answer, "distractor": distractor})
+    
+    samples = []
+    instruction = "1. Track the belief of each character as described in the story. 2. A character's belief is formed only when they perform an action themselves or can observe the action taking place. 3. A character does not have any belief about the container or its content which they cannot observe directly. 4. To answer the question, predict only the final state of the queried container in fewest tokens possible, strictly based on the belief of the character, mentioned in the question. 5. Do not predict the entire sentence with character or container as the final output."
+
+    for idx in range(n_samples):
+        alt_tb_ans = true_stories[idx]["answer"].split()
+        alt_fb_ans = false_stories[idx]["answer"].split()
+        alt_tb_ans, alt_fb_ans = get_tb_fb_answer(alt_tb_ans, alt_fb_ans)
+
+        alt_question = true_stories[idx]["question"]
+        alt_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[idx]['story']}\nQuestion: {alt_question}\nAnswer:"
+        alt_ans = alt_tb_ans
+
+        # Sample a different story
+        random_sample_idx = random.choice(range(len(true_stories)))
+        while random_sample_idx == idx:
+            random_sample_idx = random.choice(range(len(true_stories)))
+        
+        org_tb_ans = true_stories[random_sample_idx]["answer"].split()
+        org_fb_ans = false_stories[random_sample_idx]["answer"].split()
+        org_tb_ans, org_fb_ans = get_tb_fb_answer(org_tb_ans, org_fb_ans)
+
+        org_question = true_stories[random_sample_idx]["question"]
+        # Replace the second word in org_question with the second word in org_question
+        org_question = org_question.split()
+        org_question[1] = alt_question.split()[1]
+        org_question = " ".join(org_question)
+        org_prompt = f"Instructions: {instruction}\n\nStory: {true_stories[random_sample_idx]['story']}\nQuestion: {org_question}\nAnswer:"
+        org_ans = "unknown"
+
+        samples.append({
+            "org_story": true_stories[random_sample_idx]['story'],
+            "org_question": org_question,
+            "org_prompt": org_prompt,
+            "org_ans": org_ans,
+            "alt_story": true_stories[idx]['story'],
+            "alt_question": alt_question,
+            "alt_prompt": alt_prompt,
+            "alt_ans": alt_ans,
+            "target": org_tb_ans
+        })
+
+    return samples
