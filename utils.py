858756eebe412405c9e413817b22fe875683f0f4
Nikhil Prakash <nikhil07prakash@gmail.com>
2025-03-27 22:57:58 -0400

    Minor changes

--- utils.py
+++ utils.py
@@ -1563,3 +1563,98 @@ def get_bigtom_query_charac(
         })
 
     return samples
+
+
+def get_source_info(
+    STORY_TEMPLATES,
+    all_characters,
+    all_containers,
+    all_states,
+    n_samples,
+    question_type="belief_question",
+):
+    clean_configs, corrupt_configs = [], []
+    samples = []
+
+    for idx in range(n_samples):
+        template_idx = 2
+        template = STORY_TEMPLATES["templates"][template_idx]
+        characters = random.sample(all_characters, 2)
+        containers = random.sample(all_containers[template["container_type"]], 2)
+        states = random.sample(all_states[template["state_type"]], 2)
+
+        sample = SampleV3(
+            template_idx=template_idx,
+            characters=characters,
+            containers=containers,
+            states=states,
+        )
+        clean_configs.append(sample)
+
+        new_states = random.sample(all_states[template["state_type"]], 2)
+        new_characters = random.sample(all_characters, 2)
+        new_containers = random.sample(all_containers[template["container_type"]], 2)
+        while new_states[0] in states or new_states[1] in states:
+            new_states = random.sample(all_states[template["state_type"]], 2)
+        while new_characters[0] in characters or new_characters[1] in characters:
+            new_characters = random.sample(all_characters, 2)
+        while new_containers[0] in containers or new_containers[1] in containers:
+            new_containers = random.sample(all_containers[template["container_type"]], 2)
+
+        sample = SampleV3(
+            template_idx=template_idx,
+            characters=list(reversed(characters)),
+            containers=list(reversed(containers)),
+            states=list(reversed(states)),
+        )
+        corrupt_configs.append(sample)
+
+    clean_dataset = DatasetV3(clean_configs)
+    corrupt_dataset = DatasetV3(corrupt_configs)
+
+    for idx in range(n_samples):
+        random_choice = random.choice([0, 1])
+
+        if question_type == "belief_question":
+            clean = clean_dataset.__getitem__(
+                idx,
+                set_container=random_choice,
+                set_character=random_choice,
+                question_type=question_type,
+            )
+            corrupt = corrupt_dataset.__getitem__(
+                idx,
+                set_container=1 ^ random_choice,
+                set_character=1 ^ random_choice,
+                question_type=question_type,
+            )
+
+        else:
+            clean = clean_dataset.__getitem__(
+                idx, set_container=random_choice, question_type=question_type
+            )
+            corrupt = corrupt_dataset.__getitem__(
+                idx, set_container=1 ^ random_choice, question_type=question_type
+            )
+
+        samples.append(
+            {
+                "clean_characters": clean["characters"],
+                "clean_objects": clean["objects"],
+                "clean_states": clean["states"],
+                "clean_story": clean["story"],
+                "clean_question": clean["question"],
+                "clean_prompt": clean["prompt"],
+                "clean_ans": clean["target"],
+                "corrupt_characters": corrupt["characters"],
+                "corrupt_objects": corrupt["objects"],
+                "corrupt_states": corrupt["states"],
+                "corrupt_story": corrupt["story"],
+                "corrupt_question": corrupt["question"],
+                "corrupt_prompt": corrupt["prompt"],
+                "corrupt_ans": corrupt["target"],
+                "target": " " + clean_configs[idx].states[1 ^ random_choice]
+            }
+        )
+
+    return samples
